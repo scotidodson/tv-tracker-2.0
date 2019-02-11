d7bd961263b07a02cfcaa96fc0bbb03b7a61351b
scotidodson <scotidodson@gmail.com>
2019-02-10 22:23:36 -0500

    added no drop div

--- main.js
+++ main.js
@@ -4,31 +4,29 @@ document.addEventListener("DOMContentLoaded", () => {
   let dragged
 
   for(const board of boards) {
-    board.addEventListener("dragover", dragover)
     board.addEventListener("dragstart", storeDragged)
-    board.addEventListener("dragenter", dragenter)
+    board.addEventListener("dragover", dragOver)
+    board.addEventListener("dragenter", dragEnter)
     board.addEventListener("drop", drop)
   }
 
   function storeDragged(e) {
-  // e.preventDefault()
-  dragged = e.target;
-  console.log('dragged =', dragged);
+    dragged = e.target;
   }
 
-  function dragover(e) {
-  e.preventDefault();
+  function dragOver(e) {
+    e.preventDefault();
   }
-  function dragenter(e) {
+
+  function dragEnter(e) {
     e.preventDefault()
-    // e.dataTransfer.setData("text/plain", e.target.id);
   }
   function drop(e) {
     e.preventDefault();
-    // const data = e.dataTransfer.getData("text");
-    // e.target.appendChild(document.getElementById(data));
-    dragged.parentNode.removeChild( dragged );
-    e.target.appendChild( dragged );
+    if ( e.target.className === "watch-column" && e.target !== dragged.parentNode ) {
+      dragged.parentNode.removeChild(dragged);
+      e.target.appendChild(dragged);
+    }
   }
 
 });
